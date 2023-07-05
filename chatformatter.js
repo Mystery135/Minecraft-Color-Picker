7e5089d62bd59aafabfa3118a73a2ce679609f65
Mystery135 <websiteemail12345678910@gmail.com>
2023-07-05 00:41:43 -0700

    intiial commit

---
+++ chatformatter.js
@@ -0,0 +1,10 @@
+const input = document.getElementById("chatinputbox");
+const log = document.getElementById("chat-output");
+
+function updateValue() {
+    if(input.value == ""){
+        log.textContent = "Start Typing!";
+    }else{
+        log.innerHTML = input.value.replace(/\n/g, "<br>");
+    }
+}
